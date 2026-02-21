082a604e29c6b790dfcb1bc2fea1911d97f2f7c9
Alexander Earley <axe6801@rit.edu>
2026-02-20 22:39:43 -0500

    Broke out config values

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-// TODO: Break out config file.
 // TODO: Generate a graph of balance over time, with multiple lines for multiple monthly extras.
 // TODO: What other graphs can we generate... total interest paid over time?
 
-const PRINCIPAL = 150000;
-const INTEREST = 5.625;
-const START_YEAR = 2025;
-const START_MONTH = 8;
-const MONTHLY = 1245.46;
-const MONTHLY_ESCROW = 381.98;
-const LUMP_SUMS = [
-    [2025, 9, 15000],
-    [2025, 10, 31500],
-    [2025, 11, 4200],
-    [2025, 12, 1500],
-    [2026, 2, 6000],
-];
-const EXTRA_MONTHLY_START = [2026, 3];
-const EXTRA_MONTHLY_OPTIONS = [0, 1000, 2000, 2500, 3000, 3500, 4000, 4500];
-const TARGET_PRINCIPAL = 20000;
+import config from "./config.json" with { type: "json" };
 
-const monthlyTowardsLoan = MONTHLY - MONTHLY_ESCROW;
-const monthlyInterest = INTEREST / 12;
+const monthlyTowardsLoan = config.loan.monthlyPayment - config.loan.monthlyEscrow;
+const monthlyInterestRate = config.loan.interest / 12 / 100;
 
 function runThing(includeLumpSums, extraMonthlyAmount) {
-    let currentYear = START_YEAR;
-    let currentMonth = START_MONTH;
+    let currentYear = config.loan.startYear;
+    let currentMonth = config.loan.startMonth;
     let interestPaid = 0;
-    let remainingPrincipal = PRINCIPAL;
+    let remainingPrincipal = config.loan.principal;
     let belowTarget = false;
 
     while (remainingPrincipal > 0) {
-        const thisMonthsInterest = remainingPrincipal * (monthlyInterest / 100);
+        const thisMonthsInterest = remainingPrincipal * monthlyInterestRate;
         const thisMonthsScheduledPrincipal = monthlyTowardsLoan - thisMonthsInterest;
         remainingPrincipal -= thisMonthsScheduledPrincipal;
 
@@ -39,7 +23,7 @@ function runThing(includeLumpSums, extraMonthlyAmount) {
         // );
 
         if (includeLumpSums) {
-            const lumpSum = LUMP_SUMS.find(
+            const lumpSum = config.extraPayments.lumpSums.find(
                 t => t[0] === currentYear && t[1] === currentMonth,
             );
             if (lumpSum) {
@@ -50,9 +34,9 @@ function runThing(includeLumpSums, extraMonthlyAmount) {
             }
 
             if (
-                currentYear > EXTRA_MONTHLY_START[0] ||
-                (currentYear === EXTRA_MONTHLY_START[0] &&
-                    currentMonth >= EXTRA_MONTHLY_START[1])
+                currentYear > config.extraPayments.extraMonthlyStart[0] ||
+                (currentYear === config.extraPayments.extraMonthlyStart[0] &&
+                    currentMonth >= config.extraPayments.extraMonthlyStart[1])
             ) {
                 remainingPrincipal -= extraMonthlyAmount;
                 // console.log(
@@ -68,10 +52,10 @@ function runThing(includeLumpSums, extraMonthlyAmount) {
             currentMonth -= 12;
         }
 
-        if (remainingPrincipal < TARGET_PRINCIPAL && !belowTarget) {
+        if (remainingPrincipal < config.target.principal && !belowTarget) {
             belowTarget = true;
             console.log(
-                `[${extraMonthlyAmount}] [${includeLumpSums ? "x" : " "}] Principal is below \$${TARGET_PRINCIPAL.toFixed(2)} in ${currentYear}/${currentMonth}. Total Interest Paid: \$${interestPaid.toFixed(2)}.`,
+                `[${extraMonthlyAmount}] [${includeLumpSums ? "x" : " "}] Principal is below \$${config.target.principal.toFixed(2)} in ${currentYear}/${currentMonth}. Total Interest Paid: \$${interestPaid.toFixed(2)}.`,
             );
         }
     }
@@ -82,6 +66,6 @@ function runThing(includeLumpSums, extraMonthlyAmount) {
 }
 
 runThing(false, 0);
-for (const extraMonthly of EXTRA_MONTHLY_OPTIONS) {
+for (const extraMonthly of config.extraPayments.extraMonthlyOptions) {
     runThing(true, extraMonthly);
 }
