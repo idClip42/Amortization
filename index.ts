b88a1c5e25ffe33281fde46607ee16e02953c85c
Alexander Earley <axe6801@rit.edu>
2026-05-09 16:46:43 -0400

    Redid the amortization logic

--- index.ts
+++ index.ts
@@ -1,73 +1,177 @@
 import config from "./config.json" with { type: "json" };
-import type { GraphPointData, YearMonth } from "./src/types.d.ts";
-import { run } from "./src/run.js";
 import { renderGraphs } from "./src/render.js";
+// import type { GraphPointData, YearMonth } from "./src/types.d.ts";
+// import { run } from "./src/run.js";
+// import { renderGraphs } from "./src/render.js";
+import { run2 } from "./src/run2.js";
+import { GraphPointData } from "./src/types.js";
 
-// TODO: Vertical line on graph indicating today?
+// // TODO: Vertical line on graph indicating today?
 
-const currentDate = new Date();
-const graphData: GraphPointData[][] = [];
+// const START_YEAR = 2025;
+// const START_MONTH = 8;
+// const PAYMENT_DAY = 1;
 
-const configOptionalEndDate: number[] | null = config.graphs.optionalEndDate;
-const optionalEndDate: YearMonth | null = configOptionalEndDate
-    ? {
-          year: configOptionalEndDate[0],
-          month: configOptionalEndDate[1],
-      }
-    : null;
+// const PRINCIPAL = 150000;
+// const MONTHLY_PAYMENT = 1245.46;
+// const MONTHLY_ESCROW = 381.98;
+// const INTEREST = 5.625;
 
-if (config.graphs.includeRaw30Year) {
-    graphData.push(
-        run(
-            "No Extra Payments",
-            config.loan,
-            {
-                lumpSums: [],
-                monthly: { start: { year: 0, month: 0 }, amount: 0 },
-            },
-            currentDate,
-            optionalEndDate
-        )
-    );
-}
+// const LUMP_SUMS_RAW: [string, number][] = [
+//     ["September 4, 2025", 15000],
+//     ["October 2, 2025", 1500],
+//     ["October 8, 2025", 30000],
+//     ["November 10, 2025", 4200],
+//     ["December 10, 2025", 1500],
+//     ["February 10, 2026", 6000],
+//     ["March 11, 2026", 3000],
+//     ["March 23, 2026", 2500],
+//     ["April 10, 2026", 3000],
+// ];
 
-const prevLumpSums = config.extraPayments.lumpSums.map(ls => ({
-    year: ls[0],
-    month: ls[1],
-    amount: ls[2],
-}));
-const lastLumpSum = prevLumpSums[prevLumpSums.length - 1];
+// const MONTHLY_PRINCIPAL = MONTHLY_PAYMENT - MONTHLY_ESCROW;
+const LUMP_SUMS = config.lumpSums.map(value => {
+    const dateString = value[0];
+    if (typeof dateString !== "string")
+        throw new Error("Invalid lump sums type");
+    const dollars = value[1];
+    if (typeof dollars !== "number") throw new Error("Invalid lump sums type");
+    const date = new Date(dateString);
+    if (isNaN(date.getTime()))
+        throw new Error(`Invalid date string: ${dateString}`);
+    return {
+        date,
+        dollars,
+    };
+});
 
-const extraMonthlyStart = {
-    year: lastLumpSum.year,
-    month: lastLumpSum.month,
-};
-if (extraMonthlyStart.month >= 12) {
-    extraMonthlyStart.month %= 12;
-    extraMonthlyStart.year++;
-}
-
-for (const extraMonthly of config.extraPayments.extraMonthlyOptions) {
-    graphData.push(
-        run(
-            `\$${extraMonthly} Extra Monthly`,
-            config.loan,
-            {
-                lumpSums: prevLumpSums,
-                monthly: {
-                    start: extraMonthlyStart,
-                    amount: extraMonthly,
-                },
-            },
-            currentDate,
-            optionalEndDate
-        )
-    );
-}
-
-renderGraphs(
-    graphData.flat<GraphPointData[][]>(),
-    config.loan,
-    config.target.principal,
-    currentDate
+const configs: {
+    name: string;
+    lumpSums: typeof LUMP_SUMS;
+    projectedLumpSumAmt: number;
+}[] = [];
+if (config.graphs.includeRaw30Year)
+    configs.push({
+        name: "No Extra Payments",
+        lumpSums: [],
+        projectedLumpSumAmt: 0,
+    });
+configs.push(
+    ...config.projectedLumpSums.options.map(pls => ({
+        name: `\$${pls}/month`,
+        lumpSums: LUMP_SUMS,
+        projectedLumpSumAmt: pls,
+    }))
 );
+
+const dataSets = configs.map(cfg => {
+    const data = run2(
+        new Date(
+            config.loan.startYear,
+            config.loan.startMonth - 1,
+            // First payment is one month after start date.
+            // We want to accrue interest
+            // but wait til next month to make a payment.
+            config.loan.paymentDay + 1
+        ),
+        config.loan.principal,
+        config.loan.interest,
+        config.loan.monthlyPayment - config.loan.monthlyEscrow,
+        config.loan.paymentDay,
+        cfg.lumpSums,
+        {
+            startDate: new Date(config.projectedLumpSums.startDate),
+            paymentDate: config.projectedLumpSums.paymentDay,
+            dollars: cfg.projectedLumpSumAmt,
+        }
+    );
+    return {
+        name: cfg.name,
+        data: data,
+    };
+});
+console.log(dataSets);
+
+// const currentDate = new Date();
+// const graphData: GraphPointData[][] = [];
+
+// const configOptionalEndDate: number[] | null = config.graphs.optionalEndDate;
+// const optionalEndDate: YearMonth | null = configOptionalEndDate
+//     ? {
+//           year: configOptionalEndDate[0],
+//           month: configOptionalEndDate[1],
+//       }
+//     : null;
+
+// if (config.graphs.includeRaw30Year) {
+//     graphData.push(
+//         run(
+//             "No Extra Payments",
+//             config.loan,
+//             {
+//                 lumpSums: [],
+//                 monthly: { start: { year: 0, month: 0 }, amount: 0 },
+//             },
+//             currentDate,
+//             optionalEndDate
+//         )
+//     );
+// }
+
+// const prevLumpSums = config.extraPayments.lumpSums.map(ls => ({
+//     year: ls[0],
+//     month: ls[1],
+//     amount: ls[2],
+// }));
+// const lastLumpSum = prevLumpSums[prevLumpSums.length - 1];
+
+// const extraMonthlyStart = {
+//     year: lastLumpSum.year,
+//     month: lastLumpSum.month,
+// };
+// if (extraMonthlyStart.month >= 12) {
+//     extraMonthlyStart.month %= 12;
+//     extraMonthlyStart.year++;
+// }
+
+// for (const extraMonthly of config.extraPayments.extraMonthlyOptions) {
+//     graphData.push(
+//         run(
+//             `\$${extraMonthly} Extra Monthly`,
+//             config.loan,
+//             {
+//                 lumpSums: prevLumpSums,
+//                 monthly: {
+//                     start: extraMonthlyStart,
+//                     amount: extraMonthly,
+//                 },
+//             },
+//             currentDate,
+//             optionalEndDate
+//         )
+//     );
+// }
+
+// renderGraphs(
+//     graphData.flat<GraphPointData[][]>(),
+//     config.loan,
+//     config.target.principal,
+//     currentDate
+// );
+
+const graphPointData: GraphPointData[] = dataSets.flatMap(ds =>
+    ds.data.map<GraphPointData>(data => ({
+        name: ds.name,
+        date: data.day,
+        remainingPrincipal: data.remainingPrincipal,
+        principalPaid: data.paidPrincipal,
+        principalPaidAdjusted: data.paidPrincipalAdjusted,
+        interestPaid: data.paidInterest,
+        interestPaidAdjusted: data.paidInterestAdjusted,
+        totalPaid: data.paidPrincipal + data.paidInterest,
+        totalPaidAdjusted:
+            data.paidPrincipalAdjusted + data.paidInterestAdjusted,
+    }))
+);
+
+renderGraphs(graphPointData, config.loan, config.target.principal, new Date());
---
+++ src/run2.ts
@@ -0,0 +1,154 @@
+import { adjustForInflation } from "./inflation.js";
+
+const TODAYS_DATE = new Date();
+
+type DateRecord = {
+    day: Date;
+    remainingPrincipal: number;
+    paidPrincipal: number;
+    paidPrincipalAdjusted: number;
+    paidPrincipalToday: number;
+    interestAccrued: number;
+    paidInterest: number;
+    paidInterestAdjusted: number;
+    paidInterestToday: number;
+    tag: "" | "payment" | "lumpSum" | "projectedLumpSum";
+};
+
+type LumpSumProjection = {
+    startDate: Date;
+    paymentDate: number;
+    dollars: number;
+};
+
+export function run2(
+    startDate: Date,
+    initialPrincipal: number,
+    interestRate: number,
+    monthlyPayment: number,
+    monthlyPaymentDay: number,
+    lumpSums: { date: Date; dollars: number }[],
+    lumpSumProjection: LumpSumProjection
+): DateRecord[] {
+    const day: Date = startDate;
+    const dailyInterest = interestRate / 365 / 100;
+
+    let remainingPrincipal = initialPrincipal;
+    let interestAcc = 0;
+    let paidPrincipal = 0;
+    let paidInterest = 0;
+    let paidPrincipalAdjusted = 0;
+    let paidInterestAdjusted = 0;
+    const records: DateRecord[] = [];
+
+    while (remainingPrincipal > 0) {
+        // Add to the interest accumulator
+        interestAcc += remainingPrincipal * dailyInterest;
+        const cachedInterest = interestAcc;
+
+        let tag: DateRecord["tag"] = "";
+        let paidPrincipalToday = 0;
+        let paidInterestToday = 0;
+
+        if (day.getDate() === monthlyPaymentDay) {
+            const todaysPayment = monthlyPayment - interestAcc;
+            paidInterest += interestAcc;
+            remainingPrincipal -= todaysPayment;
+            paidPrincipal += todaysPayment;
+
+            const inflInput = {
+                year: day.getFullYear(),
+                month: day.getMonth() + 1,
+            };
+            const inflTarget = {
+                year: TODAYS_DATE.getFullYear(),
+                month: TODAYS_DATE.getMonth() + 1,
+            };
+            paidPrincipalAdjusted += adjustForInflation({
+                input: {
+                    ...inflInput,
+                    dollars: todaysPayment,
+                },
+                target: inflTarget,
+            });
+            paidInterestAdjusted += adjustForInflation({
+                input: {
+                    ...inflInput,
+                    dollars: interestAcc,
+                },
+                target: inflTarget,
+            });
+
+            interestAcc = 0;
+            tag = "payment";
+
+            // console.log(
+            //     day.toLocaleDateString(),
+            //     Math.round(interestAcc),
+            //     Math.round(todaysPayment),
+            //     Math.round(remainingPrincipal),
+            //     Math.round(paidInterest),
+            //     Math.round(paidPrincipal),
+            //     Math.round(paidPrincipalAdjusted),
+            //     Math.round(paidInterestAdjusted),
+            //     tag
+            // );
+        }
+
+        for (const lumpSum of lumpSums) {
+            const sameDay =
+                day.toLocaleDateString() === lumpSum.date.toLocaleDateString();
+            if (!sameDay) continue;
+            remainingPrincipal -= lumpSum.dollars;
+            paidPrincipal += lumpSum.dollars;
+            paidPrincipalToday += lumpSum.dollars;
+            tag = "lumpSum";
+
+            // console.log(
+            //     day.toLocaleDateString(),
+            //     Math.round(interestAcc),
+            //     Math.round(paidPrincipalToday),
+            //     Math.round(remainingPrincipal),
+            //     Math.round(paidPrincipal),
+            //     tag
+            // );
+        }
+
+        if (day.getTime() > lumpSumProjection.startDate.getTime()) {
+            if (day.getDate() === lumpSumProjection.paymentDate) {
+                remainingPrincipal -= lumpSumProjection.dollars;
+                paidPrincipal += lumpSumProjection.dollars;
+                paidPrincipalToday += lumpSumProjection.dollars;
+                tag = "projectedLumpSum";
+
+                // console.log(
+                //     day.toLocaleDateString(),
+                //     Math.round(interestAcc),
+                //     Math.round(paidPrincipalToday),
+                //     Math.round(remainingPrincipal),
+                //     Math.round(paidPrincipal),
+                //     tag
+                // );
+            }
+        }
+
+        records.push({
+            day: new Date(day),
+            remainingPrincipal,
+            paidPrincipal,
+            paidPrincipalToday,
+            paidInterest,
+            paidInterestToday,
+            interestAccrued: cachedInterest,
+            paidPrincipalAdjusted,
+            paidInterestAdjusted,
+            tag,
+        });
+
+        // Advance the date.
+        day.setDate(day.getDate() + 1);
+    }
+
+    // console.table(records);
+    return records;
+}
